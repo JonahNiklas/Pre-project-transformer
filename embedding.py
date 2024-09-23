91356903a8835f7ab6a332f553ca83b520d2ed31
sondresorbye0111 <sondre@magson.no>
2024-09-23 14:05:30 +0200

    tokenization

--- embedding.py
+++ embedding.py
@@ -1,13 +1,13 @@
 import numpy as np
 import logging
 from constants import transformer_config, embedding_dimension
-
+import nltk
 
 logger = logging.getLogger(__name__)
 
 
 # Load GloVe embeddings
-def load_glove(file_path):
+def _load_glove(file_path):
     logger.info(f"Loading GloVe embeddings from {file_path}")
     embeddings = {}
     with open(file_path, "r", encoding="utf-8") as f:
@@ -21,11 +21,25 @@ def load_glove(file_path):
 
 
 glove_path = f"./glove/glove.6B.{embedding_dimension}d.txt"
-glove_embeddings = load_glove(glove_path)
+glove_embeddings = _load_glove(glove_path)
 
 
-def embed_descriptions(descriptions):
-    embeddings = np.array([get_sentence_embedding(desc) for desc in descriptions])
+def embed_descriptions(descriptions: str):
+    embeddings = np.array([_get_sentence_embedding(desc) for desc in descriptions])
+
+    # Calculate the percentage of words not found in embeddings
+    total_words = sum(len(tokenize_text(desc)) for desc in descriptions)
+    missing_words = sum(
+        1
+        for desc in descriptions
+        for word in tokenize_text(desc)
+        if word not in glove_embeddings
+    )
+    missing_percentage = (missing_words / total_words) * 100
+    logger.debug(
+        f"Percentage of words not found in embeddings: {missing_percentage:.2f}%"
+    )
+
     assert embeddings.shape == (
         len(descriptions),
         transformer_config.max_seq_length,
@@ -34,9 +48,15 @@ def embed_descriptions(descriptions):
     return embeddings
 
 
-def get_sentence_embedding(description):
-    description = description[: transformer_config.max_seq_length]
-    words = description.split()
+def tokenize_text(text) -> list[str]:
+    tokens = nltk.word_tokenize(text)
+    tokens = [token.lower() for token in tokens]
+    return tokens
+
+
+def _get_sentence_embedding(description: str):
+    tokens = tokenize_text(description)
+    words = tokens[: transformer_config.max_seq_length]
     word_embeddings = [
         (
             glove_embeddings[word]
@@ -45,8 +65,6 @@ def get_sentence_embedding(description):
         )
         for word in words
     ]
-    if not word_embeddings:
-        raise Exception(f"No embeddings found for description: {description}")
     word_embeddings = np.pad(
         word_embeddings,
         ((0, transformer_config.max_seq_length - len(word_embeddings)), (0, 0)),
@@ -58,7 +76,8 @@ def get_sentence_embedding(description):
 
 if __name__ == "__main__":
     # Example usage
+    logging.basicConfig(level=logging.DEBUG)
     sentence = "The quick brown fox jumps over the lazy dog."
-    embedding = get_sentence_embedding(sentence)
+    embedding = embed_descriptions([sentence])
     print(f"Sentence embedding shape: {embedding.shape}")
     print(f"Sentence embedding: {embedding[:5]}...")  # Print first 5 values
--- get_data.py
+++ get_data.py
@@ -5,13 +5,18 @@ from constants import selected_columns
 
 import logging
 
+from embedding import tokenize_text
 from utils.bool_string import bool_string
+
 logger = logging.getLogger(__name__)
 
 
-def get_data(raw_data_path: str, save_path="data/preprocessed_data.parquet"):
-    USE_CACHE = bool_string(os.getenv("USE_CACHE", "true"))
-    if USE_CACHE and os.path.exists(save_path):
+def get_data(
+    raw_data_path: str,
+    save_path="data/preprocessed_data.parquet",
+    use_cache=bool_string(os.getenv("USE_CACHE", "true")),
+):
+    if use_cache and os.path.exists(save_path):
         logger.info(f"Loading preprocessed data from {save_path}")
         return pd.read_parquet(save_path)
 
@@ -21,6 +26,8 @@ def get_data(raw_data_path: str, save_path="data/preprocessed_data.parquet"):
 
     filtered_data = _process_dates_and_credit_age(filtered_data)
     filtered_data = _calculate_revolving_income_ratio(filtered_data)
+    filtered_data = _clean_description(filtered_data)
+    filtered_data = _add_description_length(filtered_data)
     filtered_data = _filter_short_descriptions(filtered_data)
     filtered_data = _drop_unnecessary_columns_and_na(filtered_data)
     filtered_data = _drop_current_and_late_loans(filtered_data)
@@ -33,11 +40,9 @@ def get_data(raw_data_path: str, save_path="data/preprocessed_data.parquet"):
 
 def _process_dates_and_credit_age(data):
     data["issue_d"] = pd.to_datetime(data["issue_d"], format="%b-%Y")
-    data["earliest_cr_line"] = pd.to_datetime(
-        data["earliest_cr_line"], format="%b-%Y")
+    data["earliest_cr_line"] = pd.to_datetime(data["earliest_cr_line"], format="%b-%Y")
     data = data.query("issue_d <= '2014-12-31'")
-    data["credit_age"] = (
-        data["issue_d"] - data["earliest_cr_line"]).dt.days / 30
+    data["credit_age"] = (data["issue_d"] - data["earliest_cr_line"]).dt.days / 30
     return data
 
 
@@ -48,12 +53,33 @@ def _calculate_revolving_income_ratio(data):
     return data
 
 
+def _clean_description(data):
+    """Example description:
+        Borrower added on 03/18/14 > Looking to consolidate debt as well as purchase new vehicle with loan.<br>
+    This function removes the 'Borrower added on [...] >' part and the '<br>' tag from the description.
+    """
+    data["desc"] = data["desc"].str.replace("<br>", "")
+    data["desc"] = data["desc"].str.replace("Borrower added on [^ ]+ > ", "")
+    data["desc"] = data["desc"].str.strip()
+
+    return data
+
+
+def _add_description_length(data):
+    data["desc"] = data["desc"].fillna("").astype(str)
+    data["desc_length"] = data["desc"].apply(lambda x: len(tokenize_text(x)))
+    return data
+
+
 def _filter_short_descriptions(data):
-    return data.query("desc.str.len() >= 20")
+    return data.query("desc_length >= 20")
 
 
 def _drop_unnecessary_columns_and_na(data):
-    return data.drop(columns=["earliest_cr_line", "total_rev_hi_lim", "issue_d"]).dropna()
+    return data.drop(
+        columns=["earliest_cr_line", "total_rev_hi_lim", "issue_d"]
+    ).dropna()
+
 
 def _drop_current_and_late_loans(data):
-    return data[data['loan_status'].isin(['Fully Paid', 'Charged Off'])]
+    return data[data["loan_status"].isin(["Fully Paid", "Charged Off"])]
