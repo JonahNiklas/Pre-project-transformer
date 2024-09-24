9f6c9ab52a3f1d923ac81eb1b5b6e17aa22391ed
sondresorbye0111 <sondre@magson.no>
2024-09-24 12:44:24 +0200

    moving dataset to gpu

--- dataset.py
+++ dataset.py
@@ -4,17 +4,18 @@ import torch
 
 from constants import target_column
 
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 class Dataset(torch.utils.data.TensorDataset):
     def __init__(self, hard_features: pd.DataFrame, embeddings: np.ndarray):
         assert len(hard_features) == len(embeddings)
         self.hard_features = torch.tensor(
             hard_features.drop(columns=[target_column]).to_numpy(dtype=np.float32), dtype=torch.float32
-        )
-        self.embeddings = torch.tensor(embeddings, dtype=torch.float32)
+        ).to(device)
+        self.embeddings = torch.tensor(embeddings, dtype=torch.float32).to(device)
         self.targets = torch.tensor(
             hard_features[target_column].values, dtype=torch.float32
-        )
+        ).to(device)
 
     def __len__(self):
         return len(self.hard_features)
