b4dcac1fdbff0d6ce4bb9fe7809731eaf045afb9
Sondre Sorbye <sondre@magson.no>
2024-11-05 14:39:14 +0100

    better logging

--- p2p_lending/main.py
+++ p2p_lending/main.py
@@ -35,7 +35,7 @@ from p2p_lending.utils.mc_dropout import predict_with_mc_dropout
 
 pd.options.mode.copy_on_write = True
 
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 logger.debug(f"Using device: {device}")
@@ -134,11 +134,11 @@ def train(model: BaseModel, training_dataset: Dataset, dev_dataset: Dataset):
         avg_loss = train_loss / len(train_loader)
         logger.info(
             f"Epoch {epoch+1}/{num_epochs}"
-            f"\nAverage Loss: {avg_loss:.4f}"
+            f": Average Loss: {avg_loss:.4f}"
             f", Dev AUC: {best_dev_auc:.4f}"
             f", Dev G-mean: {best_dev_gmean:.4f}"
-            f"\nAleatoric error correlation: {aleatoric_error_correlation:.4f}"
-            f"\nEpistemic error correlation: {epistemic_error_correlation:.4f}"
+            f", Aleatoric error correlation: {aleatoric_error_correlation:.4f}"
+            f", Epistemic error correlation: {epistemic_error_correlation:.4f}"
         )
         model.load_state_dict(
             torch.load(f"p2p_lending/model_weights/{model.__class__.__name__}.pth")
@@ -178,11 +178,11 @@ def evaluate(model: BaseModel, test_dataset: Dataset, check_correlation: bool = 
         return auc, gmean, None
 
     aleatoric_stds = torch.sqrt(torch.exp(aleatoric_log_variances))
-    logger.info(
+    logger.debug(
         f"Aleatoric mean, min & max STD [{aleatoric_stds.mean():.4f}, {aleatoric_stds.min():.4f}, {aleatoric_stds.max():.4f}]"
     )
     epistemic_stds = torch.sqrt(epistemic_variances)
-    logger.info(
+    logger.debug(
         f"Epistemic mean, min & max STD [{epistemic_stds.mean():.4f}, {epistemic_stds.min():.4f}, {epistemic_stds.max():.4f}]"
     )
     error = torch.abs(probas - torch.tensor(targets))
